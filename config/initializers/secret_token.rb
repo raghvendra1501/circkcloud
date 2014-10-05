04db8bb7edc354ba00ee72f6216dbbf70c701cc1
raghvendra1501 <raghvendra1501@gmail.com>
2014-10-05 21:03:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crickcloud::Application.config.secret_key_base = '7e630123255fa597e5663824517812bc8735461554894679a9588bc4b7e8b6653b010fd5393268d130e4dd6bff7883041b187dc728464b88652589c1f04befcb'
